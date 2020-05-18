088edbc500eb900dc295a6764467abf8241a72cd
young ban <ban1990@naver.com>
2020-05-18 17:39:58 +0900

    portfolio basicSettings(scss, navbar, file structure)

---
+++ src/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+import Header from "./Components/Header";
+
+const Landing = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Landing;
